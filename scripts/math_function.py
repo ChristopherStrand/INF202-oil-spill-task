35b902d7686f9f9a4470dfa622c38863879b6073
chris2pher1503 <christopher150304@outlook.com>
2025-01-06 17:35:24 +0100

    added g function

--- scripts/math_function.py
+++ scripts/math_function.py
@@ -13,36 +13,45 @@ def velocity(x_n: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
 def midpoint(coordinates: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
     return (1/3)*(coordinates[0]+coordinates[1]+coordinates[2])
 
+def unit_normal_vector(coordinates: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+
 """ def area() -> np.float32:
     return 0.5*abs(()) """
 
 def calculate_area(points: list) -> float:
-    x0, y0 = points[0].x, points[0].y
-    x1, y1 = points[1].x, points[1].y
-    x2, y2 = points[2].x, points[2].y
+    x0, y0 = points[0]
+    x1, y1 = points[1]
+    x2, y2 = points[2]
     
     return 0.5 * abs((x0 - x2)*(y1 - y0) - (x0 - x1)*(y2 - y0))
+
+def g(a , b , v, w):
+    dot_product = np.dot(v, w)
+    
+    if dot_product > 0 :
+        return a * dot_product
+    else:
+        return b * dot_product
 
 
 # calculating the change of oil in cell
 def calculate_change(cell: object, neighbors: list[int]) -> int: 
     #kan bli et problem å hente ut punktene, sjekke getter for celle og str metode for point
-    points = cell.points
-    print(points)
-    area = calculate_area(points)
-    
+    area = calculate_area( cell.points)
     sum = 0
-    for neighbor in neighbors:
-        sum += 
+    for neighbor in cell.neighbors:
+        mid = midpoint(cell.points)
+        v_mid = velocity(mid)
     
     
     
     
-def calculate_flux( , A):
-    F = -dt/A 
+def calculate_flux(oil_amount, neighbours_oil_amount , A):
+    nv = unit_normal_vector(cell.points)
+    v
+    F = -dt/A * g(oil_amount, neighbours_oil_amount, v_mid, nv)
     
-def g(a , b , v, w):
-    if 
+
     
     
 
