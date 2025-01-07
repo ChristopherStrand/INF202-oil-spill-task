b61d9623d5a6930287c4315a0df9cf264c26c3c5
Ludvik Høibjerg-Aslaksen <Ludvik253@gmail.com>
2025-01-07 16:47:06 +0100

    used numpy for finding neighbors

--- scripts/classes.py
+++ scripts/classes.py
@@ -20,7 +20,7 @@ class Point:
         return self._coordinates
 
 class Cell:
-    def __init__(self, index: int, points: list[Point]) -> None:
+    def __init__(self, index: int, points: npt.NDArray[np.float32]) -> None:
         """
         Initiliazes a Cell with ID (its position in the Mesh Cells list)
         and a list of points for the Cell
@@ -60,12 +60,12 @@ class Cell:
 
 
 class Triangle(Cell):
-    def __init__(self, index: int, points: list[Point]) -> None:
+    def __init__(self, index: int, points: npt.NDArray[np.float32]) -> None:
         super().__init__(index, points)
 
 
 class Line(Cell):
-    def __init__(self, index: int, points: list[Point]) -> None:
+    def __init__(self, index: int, points: npt.NDArray[np.float32]) -> None:
         super().__init__(index, points)
 
 
@@ -99,12 +99,11 @@ class Mesh:
             3: Triangle
             }
 
-        points = [self._points[i] for i in cell]
+        points = np.array([self._points[i] for i in cell])
         self._cell_index += 1
         
         return cell_map[cell_check](self._cell_index, points)
             
-
     def find_neighbors(self, cell_index: int) -> None:
         """
         Finds neighboring cells for the cell specified.
@@ -114,17 +113,11 @@ class Mesh:
         points_in_cell = self._cells[cell_index].points()
         
         #Checks triangles for neighbors
-        if len(points_in_cell) == 3:
-            #The points of the current cell, avoiding using a for loop to save performance
-            point_0 = points_in_cell[0].index()
-            point_1 = points_in_cell[1].index()
-            point_2 = points_in_cell[2].index()
+        if points_in_cell.size == 3: 
+            point_indicies = [point.index() for point in points_in_cell]
             for cells in self._cells:
-                point_indices = [points.index() for points in cells.points()]
-                #Checks if two different cells has exactly two points equal eachother. 
-                #This is done by taking the sum of three boolean statements that are true if the cell, 
-                #whose neighbors are being checked, have a point that is equal in the cell being checked.
-                if sum((point_0 in point_indices, point_1 in point_indices, point_2 in point_indices)) == 2:
+                point_indicies_check = [point.index() for point in cells.points()]
+                if np.intersect1d(point_indicies, point_indicies_check).size == 2: 
                     neighboring_cells.append(cells.index())
 
             #Store neighbors in each cell
--- scripts/main.py
+++ scripts/main.py
@@ -2,10 +2,16 @@ import meshio
 import classes
 import numpy as np
 
+#temp function
+def indexify(point_list):
+    return [i.index() for i in point_list]
+
 mesh = classes.Mesh("meshes/bay.msh")
 # x = np.array(mesh._cells)
 # print(x.dtype)
 # print(x[x.dtype == classes.Triangle])
-
+cells = mesh._cells
+print(f"2056: {indexify(cells[2056].points())}, 45: {indexify(cells[45].points())}, 46: {indexify(cells[46].points())}")
 mesh.find_neighbors(4)
 mesh.print_neighbors(4)
+
