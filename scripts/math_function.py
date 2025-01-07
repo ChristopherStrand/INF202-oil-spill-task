eb121a3262a04530029903eb4a5cb1441365d6a6
chris2pher1503 <christopher150304@outlook.com>
2025-01-07 12:15:32 +0100

    added scaled normal vector

--- scripts/math_function.py
+++ scripts/math_function.py
@@ -1,68 +1,74 @@
 """
 This file contains the mathmatical functions used in main.py
 """
+
 import numpy as np
 import numpy.typing as npt
 
-x_star = (0.35, 0.45) #Should we transpose? Always this value??? Intial start position
+x_star = (0.35, 0.45)  # Intial start position
 
-#Index 0 represents x value in the position vector x, and index 1 represents the y value in the position vector x
+# Index 0 represents x value in the position vector x, and index 1 represents the y value in the position vector x
 
-#Same as function u from task description. Should not return a vector
-def oil_distro(t: float, x_n: npt.NDArray[np.float32]) -> np.float32: 
-    return np.e**(-(((x_n[0]-x_star[0])**2+(x_n[1]-x_star[1])**2)/(0.01)))
 
-#Same as function v from task description. Should return a vector
-def velocity(x_n: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]: 
-    return (x_n[1]-0.2*[x_n[0]], -x_n[0])
+# Same as function u from task description. Should not return a vector
+def oil_distro(t: float, x_n: npt.NDArray[np.float32]) -> np.float32:
+    return np.e ** (-(((x_n[0] - x_star[0]) ** 2 + (x_n[1] - x_star[1]) ** 2) / (0.01)))
 
-#Same as X_mid from task description. Should return a vector
+
+# Same as function v from task description. Should return a vector
+def velocity(x_n: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+    return (x_n[1] - 0.2 * [x_n[0]], -x_n[0])
+
+
+# Same as X_mid from task description. Should return a vector
 def midpoint(coordinates: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
-    return (1/3)*(coordinates[0]+coordinates[1]+coordinates[2])
+    return (1 / 3) * (coordinates[0] + coordinates[1] + coordinates[2])
 
-def unit_normal_vector(coordinates: npt.NDArray[np.float32]) -> npt.NDArray[np.float32]:
+
+# takes in two point(sharing with neighbour) and returns a numpy array/vector
+def unit_normal_vector(point1, point2) -> npt.NDArray[np.float32]:
+    vector = point2 - point1
+    normal_vector = np.array([-vector[1], vector[0]])
+    return normal_vector / np.linalg.norm(normal_vector)
+
 
 """ def area() -> np.float32:
     return 0.5*abs(()) """
+
 
 def calculate_area(points: list) -> float:
     x0, y0 = points[0]
     x1, y1 = points[1]
     x2, y2 = points[2]
-    
-    return 0.5 * abs((x0 - x2)*(y1 - y0) - (x0 - x1)*(y2 - y0))
 
-def g(a , b , v, w):
+    return 0.5 * abs((x0 - x2) * (y1 - y0) - (x0 - x1) * (y2 - y0))
+
+
+def g(a, b, v, w):
     dot_product = np.dot(v, w)
-    
-    if dot_product > 0 :
+
+    if dot_product > 0:
         return a * dot_product
     else:
         return b * dot_product
 
 
 # calculating the change of oil in cell
-def calculate_change(cell: object, neighbors: list[int]) -> int: 
-    #kan bli et problem å hente ut punktene, sjekke getter for celle og str metode for point
-    area = calculate_area( cell.points)
+def calculate_change(cell: object, neighbors: list[int]) -> int:
+    # kan bli et problem å hente ut punktene, sjekke getter for celle og str metode for point
+    area = calculate_area(cell.points)
     sum = 0
-    for neighbor in cell.neighbors:
-        mid = midpoint(cell.points)
-        v_mid = velocity(mid)
-    
-    
-    
-    
-def calculate_flux(oil_amount, neighbours_oil_amount , A):
-    nv = unit_normal_vector(cell.points)
-    v
-    F = -dt/A * g(oil_amount, neighbours_oil_amount, v_mid, nv)
-    
+    for neighbour in cell.neighbors:
+        mid_cell = midpoint(cell.points)
+        mid_ngh = midpoint(neighbour.points)
+        v_mid = (velocity(mid_cell) + velocity(mid_ngh)) / 2
+        sum = sum + calculate_flux(
+            cell.oil_amount,
+            neighbour.oil_amount,
+            v_mid,
+        )
+    return sum
 
-    
-    
 
-    
-    
-    
-
+def calculate_flux(oil_amount, neighbours_oil_amount, v_mid, nv):
+    return -dt / A * g(oil_amount, neighbours_oil_amount, v_mid, nv)
