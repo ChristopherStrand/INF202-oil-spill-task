eae7ec93ae7464e75e54c187f93073b5a9388b97
fredericstrand <strandfrederic@outlook.com>
2025-01-10 12:13:55 +0100

    just added simple print statement to monitor inital_cell oil amount, but amount not changing

--- scripts/main.py
+++ scripts/main.py
@@ -15,9 +15,11 @@ initial_cell = math_function.find_initial_cell([0.35, 0.45], mesh._cells)
 print(initial_cell)
 print(mesh.find_neighbors(initial_cell))
 
+print(mesh.cells[initial_cell].oil_amount)
 for i in range(1, 20):
     dt = i / 10
     math_function.calculate_change(mesh, initial_cell, dt)
+    print(mesh.cells[initial_cell].oil_amount)
 # finding starting cell
 # find neighbors
 # calculate_change
