8c4dc88f5af7af4cd903760b1be88dc9bc0a889f
Ludvik Høibjerg-Aslaksen <Ludvik253@gmail.com>
2025-01-12 19:32:46 +0100

    Fix: Find_neighbors works

--- scripts/main.py
+++ scripts/main.py
@@ -5,7 +5,7 @@ import time
 import math_function
 import plotting
 
-mesh = classes.Mesh("../meshes/bay.msh")
+mesh = classes.Mesh("meshes/bay.msh")
 cells = mesh.cells
 dt = 0.1
 start_point = [0.35, 0.45]
@@ -16,8 +16,10 @@ initial_cell = math_function.find_initial_cell(cells, start_point)
 # prints to check if reading and checking neighbors works
 print(initial_cell)
 print(mesh.find_neighbors(initial_cell))
+
+mesh.find_neighbors(4)
 mesh.print_neighbors(4)
-print(mesh.find_neighbors(4))
+
 
 
 print(mesh.cells[initial_cell].oil_amount)
