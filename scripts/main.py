a51cd67902973298358608ff15121d7023647566
chris2pher1503 <christopher150304@outlook.com>
2025-01-12 19:27:14 +0100

    fix/ problems with inputs in funcstions

--- scripts/main.py
+++ scripts/main.py
@@ -16,12 +16,17 @@ initial_cell = math_function.find_initial_cell(cells, start_point)
 # prints to check if reading and checking neighbors works
 print(initial_cell)
 print(mesh.find_neighbors(initial_cell))
+mesh.print_neighbors(4)
+print(mesh.find_neighbors(4))
+
 
 print(mesh.cells[initial_cell].oil_amount)
 for i in range(1, 20):
     dt = i / 10
     math_function.calculate_change(mesh, initial_cell, dt)
     print(mesh.cells[initial_cell].oil_amount)
+
+plotting.plotting_mesh(cells)
 
 # finding starting cell
 # find neighbors
