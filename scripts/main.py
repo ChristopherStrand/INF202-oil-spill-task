8765d082cbbfad8f6a2076f0241f09e6d31f0877
fredericstrand <strandfrederic@outlook.com>
2025-01-13 14:57:20 +0100

    forgot to pull from main

--- scripts/main.py
+++ scripts/main.py
@@ -5,43 +5,24 @@ import time
 import math_function
 import plotting
 
-mesh = classes.Mesh("../meshes/bay.msh")
+mesh = classes.Mesh("./meshes/bay.msh")
 cells = mesh.cells
 dt = 0.1
 start_point = [0.35, 0.45]
 
-math_function.initial_oil_distribution(cells, start_point)
-initial_cell = math_function.find_initial_cell(cells, start_point)
+def solve(cells, start_point):
+    math_function.initial_oil_distribution(cells, start_point)
+    initial_cell = math_function.find_initial_cell(cells, start_point)
 
-# prints to check if reading and checking neighbors works
-print(initial_cell)
-print(mesh.find_neighbors(initial_cell))
+    for i in range(1,20):
+        dt = i / 10
+        timestep()
 
-mesh.find_neighbors(4)
-mesh.print_neighbors(4)
+def timestep():
+    for cell in mesh.cells:
+        print(cell)
+        mesh.find_neighbors(cell)
+        for ngh in cell.neighbors:
+            math_function.calculate_change(mesh, cell_index, dt)
 
-
-print(mesh.cells[initial_cell].oil_amount)
-for i in range(1, 20):
-    dt = i / 10
-    math_function.calculate_change(mesh, initial_cell, dt)
-    """ print(mesh.cells[initial_cell].oil_amount) """
-
-plotting.plotting_mesh(cells)
-
-# finding starting cell
-# find neighbors
-# calculate_change
-
-# if oil != 0 in neighbor
-# find neighbors for each neighbor of original cell
-# calculate change
-
-# if oil != 0 in neighbor
-# find neighbors for each neighbor of original cell
-# calculate change
-
-
-""" initialneighbors = mesh.find_neighbors(first_cell) """
-
-""" math_function.calculate_change(5) """
+solve(cells, start_point)
