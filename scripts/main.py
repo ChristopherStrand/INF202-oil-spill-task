f9628b29e37715eef34edf4ec3fce04297dc3d11
chris2pher1503 <christopher150304@outlook.com>
2025-01-13 15:57:24 +0100

    removing some unecessary comments

--- scripts/main.py
+++ scripts/main.py
@@ -25,23 +25,7 @@ print(mesh.cells[initial_cell].oil_amount)
 for i in range(1, 20):
     dt = i / 10
     math_function.calculate_change(mesh, initial_cell, dt)
-    """ print(mesh.cells[initial_cell].oil_amount) """
-
-plotting.plotting_mesh(cells)
-
-# finding starting cell
-# find neighbors
-# calculate_change
-
-# if oil != 0 in neighbor
-# find neighbors for each neighbor of original cell
-# calculate change
-
-# if oil != 0 in neighbor
-# find neighbors for each neighbor of original cell
-# calculate change
 
 
-""" initialneighbors = mesh.find_neighbors(first_cell) """
-
-""" math_function.calculate_change(5) """
+""" plotting functions needs two arguments: cells and time step, this is for naming the plots images """
+plotting.plotting_mesh(cells, 1)
